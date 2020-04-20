d384c1fa78fd64b4fec0db59aa94aaf61118e150
Harry887 <761228040@qq.com>
2020-04-20 14:16:49 +0000

    quasi-antagonism r101 baseline

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -9,10 +9,11 @@ from .voc import VOCDataset
 from .wider_face import WIDERFaceDataset
 from .xml_style import XMLDataset
 from .nwpu import NWPUDataset
+from .detrac import DETRACDataset
 
 __all__ = [
     'CustomDataset', 'XMLDataset', 'CocoDataset', 'VOCDataset',
     'CityscapesDataset', 'GroupSampler', 'DistributedGroupSampler',
     'build_dataloader', 'ConcatDataset', 'RepeatDataset', 'WIDERFaceDataset',
-    'DATASETS', 'build_dataset', 'NWPUDataset'
+    'DATASETS', 'build_dataset', 'NWPUDataset', 'DETRACDatasets'
 ]
